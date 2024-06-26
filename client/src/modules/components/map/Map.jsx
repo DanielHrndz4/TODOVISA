1926216a3f79347674022d028461d0b829845907
danielhrndz <darwinhrndz12@gmail.com>
2024-06-26 14:32:23 -0600

    add: responsive design

--- client/src/modules/components/map/Map.jsx
+++ client/src/modules/components/map/Map.jsx
@@ -36,7 +36,7 @@ export default function MapComponent() {
       </div>
       <Fade>
         <div className="w-full flex lg:px-0 px-6">
-          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:h-[55px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
+          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:min-w-[55px] lg:h-[55px] w-[45px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
             <FontAwesomeIcon icon={faLocationDot} size="x lg:2x" className="w-full m-auto text-white" />
           </div>
           <a href="https://www.google.com/maps/dir//67+Avenida+Sur+Local+%231,+San+Salvador/@13.6970016,-89.2252546,18z/data=!4m8!4m7!1m0!1m5!1m1!1s0x8f6331d77b1013e3:0xbfa86a56cf477af7!2m2!1d-89.2246802!2d13.6971043?entry=ttu" className="flex justify-center items-center ">
@@ -48,7 +48,7 @@ export default function MapComponent() {
       </Fade>
       <Fade>
         <div className="w-full flex mt-4  lg:px-0 px-6">
-          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:h-[55px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
+          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:min-w-[55px] lg:h-[55px] w-[45px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
             <FontAwesomeIcon icon={faPhone} size="x lg:2x" className="w-full m-auto text-white" />
           </div>
           <span className="pl-4 lg:text-xl text-lg flex items-center justify-center text-white">
@@ -56,7 +56,7 @@ export default function MapComponent() {
           </span>
         </div>
         <div className="w-full flex mt-4  lg:px-0 px-6">
-          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:h-[55px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
+          <div className="border-2 border-white p-2 rounded-full lg:w-[55px] lg:min-w-[55px] lg:h-[55px] w-[45px] min-w-[45px] h-[45px] flex justify-center items-center shadowbtn">
             <FontAwesomeIcon icon={faClock} size="x lg:2x" className="w-full m-auto text-white" />
           </div>
           <span className="pl-4 lg:text-xl text-lg flex items-center justify-center text-white flex-row">
