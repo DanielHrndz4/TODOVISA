6979e858660f9a0072255c9ae0a72cb7e6647258
“DanielHrndz” <00015322@uca.edu.sv>
2024-06-26 22:33:31 -0600

    add: production api

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const connectDB = require('./database/database');
 const userRoute = require('./routes/user.route')
 const app = express();
 const cors = require('cors');
+const session = require('express-session');
 const PORT = process.env.PORT || 3333;
 
 // Conectar a la base de datos
@@ -12,10 +13,20 @@ connectDB();
 
 // Configura CORS para permitir cualquier origen
 app.use(cors({
-  origin: '*',
+  origin: 'https://todovisa.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use(session({
+  secret: 'your_secret_key', // Replace with your secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: true, // Use true if your site is HTTPS
+    sameSite: 'none' // Ensure cross-site requests work
+  }
 }));
 
 app.use(cookieParser())
