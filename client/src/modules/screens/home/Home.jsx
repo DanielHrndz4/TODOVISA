ff8fab42544b0ccc6c5f1985d2fbc47a748d60d6
danielhrndz <darwinhrndz12@gmail.com>
2024-06-27 09:34:49 -0600

    add: responsive design

--- client/src/modules/screens/home/Home.jsx
+++ client/src/modules/screens/home/Home.jsx
@@ -24,7 +24,6 @@ export default function Home() {
           credentials: 'include', 
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
           }
         }
       );
--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(cors({
   origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type']
 }));
 
 app.use(cookieParser())
