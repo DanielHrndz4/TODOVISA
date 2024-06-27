7f4022923b3555ffa685b5d5a5d9358f933ccf54
“DanielHrndz” <00015322@uca.edu.sv>
2024-06-26 23:07:22 -0600

    add: production api

--- server/app.js
+++ server/app.js
@@ -12,12 +12,7 @@ const SECRET_KEY = process.env.SECRET_KEY;
 connectDB();
 
 // Configura CORS para permitir cualquier origen
-app.use(cors({
-  origin: 'https://todovisa.onrender.com/',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use(cookieParser())
 app.use(express.json());
