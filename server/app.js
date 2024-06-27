a117f8d6225feaf901c2bcd6d7e1ac7ff69fea10
danielhrndz <darwinhrndz12@gmail.com>
2024-06-27 09:11:33 -0600

    add: fetching with server

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ connectDB();
 
 // Configura CORS para permitir cualquier origen
 app.use(cors({
-  origin: 'https://todovisa.onrender.com/api',
+  origin: 'https://todovisa.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
