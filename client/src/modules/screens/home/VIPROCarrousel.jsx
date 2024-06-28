9078757fd234050b67bd252e728cfca464931e2c
danielhrndz <darwinhrndz12@gmail.com>
2024-06-28 17:03:44 -0600

    add: lang

--- client/src/modules/screens/home/VIPROCarrousel.jsx
+++ client/src/modules/screens/home/VIPROCarrousel.jsx
@@ -5,24 +5,39 @@ import "slick-carousel/slick/slick-theme.css";
 import { Button } from "@material-tailwind/react";
 import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
-import lang from "../../../assets/data/lang.data"
+import lang from "../../../assets/data/lang.data";
 
-function VIPROCarrousel() { 
+function VIPROCarrousel() {
   const countries = Object.values(lang[0].about.countries);
+
   const carrouselData = () => {
     return countries.map((country, index) => (
       <Fade key={index}>
         <div className="mx-2">
-          <div className="w-[90%] m-auto bg-white p-4 rounded-md shadow my-12">
-            <img src={country.img} alt={country.name} className="max-h-[13rem] w-full h-[12rem] sm:h-[12rem] lg:h-[11rem] xl:h-[12rem] rounded-md" />
-            <h1 className="text-xl lg:text-2xl font-semibold pt-3 text-center">{country.name}</h1>
-            <p className="py-2 xl:min-h-[9rem] sm:min-h-[10rem] lg:min-h-[11rem] min-h-[10rem]">{country.description}</p>
-            <Link to={country.uri}><Button className="font-semibold w-full bg-TVred shadowbtn">Ver más</Button></Link>
+          <div className="w-full sm:w-[90%] m-auto bg-white p-4 rounded-md shadow my-12">
+            <div className="square-image-container">
+              <img
+                src={country.img}
+                alt={country.name}
+                className="square-image"
+              />
+            </div>
+            <h1 className="text-xl lg:text-2xl font-semibold pt-3 text-center">
+              {country.name}
+            </h1>
+            <p className="py-2 xl:min-h-[9rem] max-h-[8rem] h-[8rem]">
+              {country.description}
+            </p>
+            <Link to={country.uri}>
+              <Button className="font-semibold w-full bg-TVred shadowbtn">
+                Ver más
+              </Button>
+            </Link>
           </div>
         </div>
       </Fade>
     ));
-  }
+  };
 
   const settings = {
     dots: false,
@@ -42,26 +57,17 @@ function VIPROCarrousel() {
         },
       },
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2,
         },
       },
       {
         breakpoint: 640,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          initialSlide: 1,
         },
       },
     ],
@@ -79,3 +85,4 @@ function VIPROCarrousel() {
 }
 
 export default VIPROCarrousel;
+
