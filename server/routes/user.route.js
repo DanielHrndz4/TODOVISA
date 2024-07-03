4ead2eb04ebdf64a2725a659894adae268c97b01
danielhrndz <darwinhrndz12@gmail.com>
2024-07-03 11:41:35 -0600

    modify: lang design

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const userSchema = require('../models/user.schema');
 const router = express.Router();
 const Form = require('../models/form.schema');
+
 require('dotenv').config();
 
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -24,6 +25,30 @@ const authenticate = (req, res, next) => {
   });
 };
 
+
+router.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+  userSchema
+    .findOne({ email: email, password: password })
+    .then((user) => {
+      if (user) {
+        const token = jwt.sign({ useremail: user.email }, SECRET_KEY);
+        res.json({
+          message: 'Inicio de sesión exitoso', token, user: {
+            email: user.email,
+            name: user.name,
+            country: user.country
+          }
+        });
+      } else {
+        res.status(401).json({ message: 'Credenciales inválidas' });
+      }
+    })
+    .catch((error) => {
+      console.error(error.message);
+      res.status(500).json({ message: 'Server error' });
+    });
+});
 
 router.post('/show-form-eeuu', (req, res) => {
   const email = req.body.email;
@@ -63,6 +88,35 @@ router.post('/update-form-eeuu', async (req, res) => {
   }
 });
 
+router.post('/signup', (req, res) => {
+  const { name, lastname, email, password, country, tel } = req.body;
+
+  userSchema.findOne({ email: email })
+    .then((existUser) => {
+      if (existUser) {
+        return res.status(400).json({ message: `El usuario con email: ${email} ya se encuentra registrado` });
+      }
+
+      const newUser = new userSchema({
+        name: name,
+        lastname: lastname,
+        email: email,
+        password: password,
+        country: country,
+        tel: tel
+      });
+
+      newUser.save()
+        .then((user) => {
+          res.status(200).json({ message: "Usuario registrado exitosamente" });
+        })
+        .catch((error) => {
+          console.error(error.message);
+          res.status(500).json({ message: 'Server error' });
+        });
+    });
+});
+
 router.post('/forms', async (req, res) => {
   const { email, formData } = req.body;
 
@@ -82,7 +136,7 @@ router.post('/forms', async (req, res) => {
   }
 });
 
-router.get('/protected-route', authenticate,(req, res) => {
+router.get('/protected-route',authenticate, (req, res) => {
   res.send('This is a protected route');
 });
 
