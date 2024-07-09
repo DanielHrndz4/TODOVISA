d09fa6ee8ebad3d0c6b06df6180e1c936e85eb6c
“DanielHrndz4” <darwinhrndz12@gmail.com>
2024-07-08 22:14:25 -0600

    add: form response eeuu

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@emotion/react/jsx-runtime']
+    }
+  },
   resolve: {
     alias: {
       '@': '/src'
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['@emotion/react/jsx-runtime']
-    }
-  }
 });
