04cb6376b03a2dd70d5bb0bb96a89bab21fd11bd
DanielHrndz <00015322@uca.edu.sv>
2024-09-02 21:43:34 +0000

    [TODOVISA Client & Server] Modify: URI

--- client/src/assets/data/admin/uri.api.js
+++ client/src/assets/data/admin/uri.api.js
@@ -1,5 +1,5 @@
-//const URI = 'https://todovisa-api.onrender.com/api'
-const URI = 'https://todovisa.onrender.com/api'
+const URI = 'https://todovisa-api.onrender.com/api'
+//const URI = 'https://todovisa.onrender.com/api'
 //const URI = 'http://localhost:3366/api'
 
 export default URI
